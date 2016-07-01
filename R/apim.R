4c75136fc4fec361d4717753300b94c5ed551987
Randi Garcia <Randi Garcia>
2016-07-01 09:49:26 -0400

    added documentation for dat and apim()

--- R/apim.R
+++ R/apim.R
@@ -1,7 +1,15 @@
-#' @
+#' @name apim
+#' @title actor-partner interdependence model
 #'
+#' will run the apim on indistinguishable dyads
+#' 
+#' even more stuff
+#'
+#' @param x is a function
+#'
+#' @details some additional details about these functions
 
-
+#' @export
 apim <- function(x, dat, dyadid, option = "indistinguishable"){
   
     if (lazyeval::is_formula(x)){
