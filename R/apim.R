c5df7f9cb283f75723d1f2e7c3794439be1657db
Randi Garcia <Randi Garcia>
2016-07-01 09:19:13 -0400

    added files, made changes, but forgot to commit

--- R/apim.R
+++ R/apim.R
@@ -1,22 +1,19 @@
-
-
-
-
+#' @
+#'
 
 
 apim <- function(x, dat, dyadid, option = "indistinguishable"){
   
-  
-  if (lazyeval::is_formula(x)){
-   
-   mod <- gls(x, na.action=na.omit, 
-        method="REML", verbose=TRUE, correlation=corCompSymm(form=~1|dyadid),
-        data=dat)
-   
-   return(mod)
-    
-  }else {
-    message("please enter a formula")
-  }
+    if (lazyeval::is_formula(x)){
+      
+      mod <- gls(x, na.action=na.omit, 
+                 method="REML", verbose=TRUE, correlation=eval(parse(text=paste0("corCompSymm(form=~1|", dyadid, ")"))),
+                 data=dat)
+      
+      return(mod)
+      
+    }else {
+      message("please enter a formula")
+    }
   
 }
