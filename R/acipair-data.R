bcc1d483093143b4c611482f81833475b682988a
RandiLGarcia <rgarcia@smith.edu>
2017-03-15 18:51:53 -0400

    added data with .RData instead of .Rda.

--- R/acipair-data.R
+++ R/acipair-data.R
@@ -4,20 +4,13 @@
 #'
 #' @docType data
 #'
-#' @usage data(grav)
+#' @usage data(acipair)
 #'
 #' @format An object of class \code{"cross"}; see \code{\link[qtl]{read.cross}}.
 #'
 #' @keywords datasets
 #'
-#' @references Moore et al. (2013) Genetics 195:1077-1086
-#' (\href{http://www.ncbi.nlm.nih.gov/pubmed/23979570}{PubMed})
-#'
-#' @source \href{http://qtlarchive.org/db/q?pg=projdetails&proj=moore_2013b}{QTL Archive}
-#'
 #' @examples
-#' data(grav)
-#' times <- attr(grav, "time")
-#' phe <- grav$pheno
+#' data(acipair)
 #' \donttest{iplotCurves(phe, times)}
-"grav"
+"acipair"
