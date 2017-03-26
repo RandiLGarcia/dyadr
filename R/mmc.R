ac42d57f989a4437e309b2be4aeb3ccbcf243623
RandiLGarcia <rgarcia@smith.edu>
2017-03-26 12:15:27 -0400

    small fixes.

--- R/mmc.R
+++ R/mmc.R
@@ -6,8 +6,8 @@
 #' 
 #' @param a path from X to M.
 #' @param b path from M to Y.
-#' @param asesq standard error for path a squared.
-#' @param bsesq standard error for path b squared.
+#' @param ase standard error for path a.
+#' @param bse standard error for path b.
 #' @param cov  covariance for a and b (set to zero in MLM, but could be free if using lavaan). Defaults to zero.
 #' @param rep  number of replications (defaults to 40,000).
 #' @param conf confidence level (defaults to 95).
@@ -16,12 +16,12 @@
 #' @details Could and maybe should output p value -- code available from last year
 #' @export 
 #'
-mmc<-function(a,b,asesq,bsesq,cov = 0,rep = 40000,conf = 95, dig = 6) {
+mmc<-function(a,b,ase,bse,cov = 0,rep = 40000,conf = 95, dig = 6) {
   require(MASS)
   
   pest <- c(a,b)
   
-  acov <- matrix(c(asesq^2,cov,cov,bsesq^2),2,2)
+  acov <- matrix(c(ase^2,cov,cov,bse^2),2,2)
   mcmc <- mvrnorm(rep,pest,acov,empirical=FALSE)
   
   ab <- mcmc[,1]*mcmc[,2]
--- R/sobel.R
+++ R/sobel.R
@@ -1,21 +1,21 @@
 #' @name sobel
 #' @title Soboel Method for Testing Idirect Effect
 #'
-#' The Sobel method is concervative. Use mmc, or bootstrapping if marginal.
+#' The Sobel method is conservative. Use mmc, or bootstrapping if marginal. The Sobel test also presumes a and b are uncorrelated. 
 #' 
 #' 
 #' @param a path from X to M.
 #' @param b path from M to Y.
-#' @param asesq standard error for path a squared.
-#' @param bsesq standard error for path b squared.
+#' @param ase standard error for path a.
+#' @param bse standard error for path b.
 #'
 #' @details Could and maybe should output p value -- code available from last year
 #' @export 
 #'
-sobel <- function(a, b, asesq, bsesq){
+sobel <- function(a, b, ase, bse){
   
   ab <- a*b
-  ab_se <- sqrt(a^2*bsesq^2+b^2*asesq^2)
+  ab_se <- sqrt(a^2*bse^2+b^2*ase^2)
   z <- ab/ab_se
   
   ifelse(ab > 0, p <- 2*pnorm(z, lower.tail=FALSE), p <- 2*pnorm(z))
