a23336a4ea1c9ecc44a3e195b7292915cf24e6fd
RandiLGarcia <rgarcia@smith.edu>
2017-06-06 14:46:29 -0400

    default for crsp

--- R/crsp.R
+++ R/crsp.R
@@ -4,12 +4,24 @@
 #' Get multiple R Squared 
 #' 
 #'
-#' @param esdmod is error standard devisiation of the model.
+#' @param esdmod is error standard deviation of the model of interest.
+#' @param esdemod is error standard deviation of the empty model.
+#' 
 #'
 #' @details some additional details about these functions
 #' @export
 #
-crsp = function (esdmod,esdemod) {
-  rsq =1 - esdmod^2/esdemod^2
-  if (rsq<0) rsq=0
-  return(rsq)}
+crsp = function (esdmod, esdemod, indistinguishable = TRUE) {
+  if(indistinguishable){
+    rsq =1 - esdmod^2/esdemod^2
+    if (rsq<0) rsq=0
+    return(rsq)
+  }
+  # 
+  # if(!indistinguishable){
+  #   rsq =1 - esdmod^2/esdemod^2
+  #   if (rsq<0) rsq=0
+  #   return(rsq)
+  # }
+  # 
+  }
