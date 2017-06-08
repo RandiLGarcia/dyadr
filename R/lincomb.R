90ed0cf504605be75b8637cd517b96d9e2e54a05
RandiLGarcia <rgarcia@smith.edu>
2017-06-08 19:29:48 -0400

    extended lincomb to work with gls.

--- R/lincomb.R
+++ R/lincomb.R
@@ -4,7 +4,7 @@
 #' Test the sum (S), the average (A), or the difference (D) of two effects from the same model.
 #' 
 #'
-#' @param outp is the summary of the model object. For exmaple, summary(mod).
+#' @param outp is the summary of the model object. For exmaple, summary(mod). It can be a gls or lme object.
 #' @param v1 is the number of the first effect.
 #' @param v2 is the number of the second effect.
 #' @param fun is the comparison. Default fun = "D". Other options include "S" the sum, and "A" the average.
@@ -13,19 +13,36 @@
 #' @export
 #'
 #
-lincomb = function (outp,v1,v2,fun="D") {
-  vval=matrix()
-  c1=as.numeric(outp$coefficients[v1])
-  c2=as.numeric(outp$coefficients[v2])
+lincomb <- function(outp,v1,v2,fun="D"){
+  vval = matrix()
+  
+  if(class(outp)[2]=="gls"){
+    c1=as.numeric(outp$coefficients[v1])
+    c2=as.numeric(outp$coefficients[v2])
+    
+    cccov =outp$varBeta[v2,v1]
+  }
+  
+  if(class(outp)[2]=="lme"){
+    c1=as.numeric(outp$coefficients$fixed[v1])
+    c2=as.numeric(outp$coefficients$fixed[v2])
+    
+    cccov =outp$varFix[v2,v1]
+  }
+  
+  c1se=outp$tTable[v1,2]
+  c2se=outp$tTable[v2,2]
+  
   if(fun=="A")vval[1]=(c1+c2)/2
   if(fun=="S")vval[1]=c1+c2
   if(fun=="D")vval[1]=c1-c2
-  c1se=outp$tTable[v1,2]
-  c2se=outp$tTable[v2,2]
-  cccov =outp$varBeta[v2,v1]
+  
   if(fun=="A")vval[2]=(c1se^2+c2se^2+2*cccov)/4
   if(fun=="S")vval[2]=c1se^2+c2se^2+2*cccov
   if(fun=="D")vval[2]=c1se^2+c2se^2-2*cccov
+  
   vval[2]=sqrt(vval[2]) 
   vval[3] = 2-2*pnorm(abs(vval[1]/vval[2]))
-  return(vval)}
+  
+  return(vval)
+}
