b9a88eeb3802458104a9fbb3cb55da88d99015c2
RandiLGarcia <rgarcia@smith.edu>
2018-05-17 11:54:33 -0400

    daves changes in getevd

--- R/getevd.R
+++ R/getevd.R
@@ -1,17 +1,25 @@
 #' @name getevd
 #' @title Error standard deviation for distinguishable dyads
 #'
-#' Get error standard deviations from the distinguishable model
+#' Get error standard deviations from the distinguishable model with gls
 #' 
 #'
-#' @param outp is the model object.
+#' @param outp is the model object.  Not it is not the "summary" of that object.
+#' @param tipe is the type of model object.  It defaults to "gls."  The other option is "lme."
 #'
 #' @details some additional details about these functions
 #' @export
 #'
-getevd = function (outp) {
+getevd = function (outp,tipe="gls",grp1="Group 1",grp2="Group 2") {
   vval=c(1,1)
-  ppp =outp[1]$modelStruct[2]
+  if (tipe=="gls")
+  {ppp =outp[1]$modelStruct[2]
   vval[2] = as.numeric(outp[6])
-  vval[1] = vval[2]/attr(ppp$varStruct,"weights")[2]
+  vval[1] = vval[2]/attr(ppp$varStruct,"weights")[2]}
+  if (tipe=="lme")
+  {ppp =outp[1]$modelStruct$varStruct
+  vval[2] = as.numeric(outp[6])
+  vval[1] = vval[2]/attr(ppp,"weights")[2]}
+  names(vval)[1]=grp1
+  names(vval)[2]=grp2
   return(vval)}
