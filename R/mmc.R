43644920d95f0fc721a6fc53cf9ad74eb4e7f4da
RandiLGarcia <rgarcia@smith.edu>
2018-05-17 11:56:11 -0400

    daves changes to mmc

--- R/mmc.R
+++ R/mmc.R
@@ -5,22 +5,24 @@
 #' @param b path from M to Y.
 #' @param ase standard error for path a.
 #' @param bse standard error for path b.
-#' @param cov  covariance for a and b (set to zero in MLM, but could be free if using lavaan). Defaults to zero.
+#' @param cov  covariance for a and b (set to zero for MLM, but could be free if using lavaan). Defaults to zero.
 #' @param rep  number of replications (defaults to 40,000).
 #' @param conf confidence level (defaults to 95).
 #' @param dig number of decimal places (defaults to 6)
+#' @param seed seed value (defaults to 12345)
 #'
-#' @details Could and maybe should output p value -- code available from last year. Base code taken from Selig and Preacher (2008).
+#' @details Base code taken from Selig and Preacher (2008).
 #' @export 
 #'
-mmc<-function(a,b,ase,bse,cov = 0,rep = 40000,conf = 95, dig = 6) {
+mmc<-function(a,b,ase,bse,cov = 0,rep = 40000,conf = 95, dig = 6, seed=12345) {
   require(MASS)
   
   pest <- c(a,b)
   
   acov <- matrix(c(ase^2,cov,cov,bse^2),2,2)
+  set.seed(seed)
   mcmc <- mvrnorm(rep,pest,acov,empirical=FALSE)
-
+  
   
   ab <- mcmc[,1]*mcmc[,2]
   
@@ -35,6 +37,7 @@ mmc<-function(a,b,ase,bse,cov = 0,rep = 40000,conf = 95, dig = 6) {
   p_value = 1-2*abs(.5- cc)
   
   CI <- cbind.data.frame(LB, UB, p_value)
+  rownames(CI)=paste0(low," to ",upp)
   return(CI)
-
+  
 }
