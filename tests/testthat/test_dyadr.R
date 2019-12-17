45cb6a61b241de754200136325b3882ae4aaca83
Jane Bang <jbang19@smith.edu>
2019-12-17 17:18:32 -0500

    testthat and examples

--- tests/testthat/test_dyadr.R
+++ tests/testthat/test_dyadr.R
@@ -3,3 +3,9 @@ context("dyadr")
 test_that("var_labels error works", {
   expect_error(var_labels(iris), "labels")
 })
+
+testthat::expect_message(apim("x"), "please enter a formula")
+
+testthat::expect_gt(crsp(esd,esd0),0.3348468)
+
+testthat::expect_equal_to_reference(smallsummary(apim), "p-value")
