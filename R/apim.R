fabf804068df8083d7b66f01d726ef5ce8a16306
Ben Baumer <ben.baumer@gmail.com>
2020-07-29 11:09:06 -0400

    lazyeval -> rlang

--- R/apim.R
+++ R/apim.R
@@ -5,17 +5,15 @@
 #'
 #' even more stuff about the apim
 #'
-#' @param x is a function with form y ~ x.
+#' @param x is a function with form \code{y ~ x}.
 #' @param dat is the dataframe.
 #' @param dyadid is a dyadic variable.
 #' @param option is to check whether dyad is indistingusihable.
 #'
 #' @details some additional details about these functions
-#' @import lazyeval
-
 #' @export
 apim <- function(x, dat, dyadid, option = "indistinguishable") {
-  if (lazyeval::is_formula(x)) {
+  if (rlang::is_formula(x)) {
     mod <- gls(x,
       na.action = na.omit,
       method = "REML", verbose = TRUE, correlation = eval(parse(text = paste0("corCompSymm(form=~1|", dyadid, ")"))),
